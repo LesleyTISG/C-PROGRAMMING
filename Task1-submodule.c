5d98b2d158540974ad48767ab08e869ee50daba4
LesleyTISG <oweles2004@gmail.com>
2025-06-07 08:14:11 +0300

    Initial commit

---
+++ Task1-submodule.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int Age = 20;
+float Height = 5.9;
+double Distance = 12345.6789;
+char Initial = 'A';
+
+printf("Age: %d\n", Age);
+printf("Height: %.2f\n", Height);
+printf("Distance: %.4lf\n", Distance);
+printf("Initial: %c\n", Initial);
+        
+    return 0;
+}
